3a5461aa54f55d572a41576a20ca4f97ae04a612
Asmit Das Joy <asmitjoy17@gmail.com>
2020-01-29 20:52:20 +0600

    Create user.php file

---
+++ User.php
@@ -0,0 +1,17 @@
+<?php
+    include_once 'Session.php';
+    include 'Database.php';
+    class User{
+
+        private $db
+
+        public function __construct(argument) {
+
+            $this->db = new Database();
+
+        }
+        
+    }
+    
+    
+?>
