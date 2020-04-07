c9ae7251ac5d4bd7a1af6a1b94ff404d0ab0661d
Asmit Das Joy <asmitjoy17@gmail.com>
2020-04-08 00:52:54 +0600

    Design Home Page

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Register System PHP</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <script src="jquery.js"></script>
+    <script src="bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <nav class = "navbar navbar-default">
+            <div class="container-fluid" >
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">Login Register System PHP and PDO</a>
+                </div>
+                <ul class="nav navbar-nav pull-right">
+                <li><a href="profile.php">Profile</a></li>
+                    <li><a href="profile.php">Logout</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="register.php">Register</a></li>
+                </ul>
+            </div>
+        </nav>
+    Body Text
+    
+    <div class="well">
+        <h3>AsmitJoy.Shovon.net
+            <span class="pull-right">Like Us:AsmitJoy</span>
+        </h3>
+    </div>
+
+    </div>
+</body>
+</html>
