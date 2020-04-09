fe39d9317999cdba3f5c2ca3b01ced8beb441f9b
Asmit Das Joy <asmitjoy17@gmail.com>
2020-04-10 00:19:14 +0600

    Seperate footer code

---
+++ footer.php
@@ -0,0 +1,10 @@
+    
+        <div class="well">
+            <h3>AsmitJoy.Shovon.net
+                <span class="pull-right">Like Us:AsmitJoy</span>
+            </h3>
+        </div>
+
+    </div>
+</body>
+</html>
