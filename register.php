2a8b63a00b50e1c3a36bd7ce290d4e997f0d6c07
Asmit Das Joy <asmitjoy17@gmail.com>
2020-04-15 01:36:21 +0600

    Email Validation

--- register.php
+++ register.php
@@ -1,10 +1,10 @@
 <!--Header-->
 <?php include 'header.php'?>
-<?php include 'user.php'?>
+<?php include 'User.php'?>
 <?
     $user = new User();
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])) {
-        $userRegi = $user->userRegistration($_POST );
+    if (isset($_POST['register'])) {
+        $userRegi = $user->userRegistration($_POST);
     }
      
 ?>
@@ -14,28 +14,34 @@
             <div class="panel-heading">
                 <h2>User Registration</h2>
             </div>
+            
             <div class="panel-body">
+            <?php
+                if (isset($userRegi)) {
+                    echo $userRegi;
+                }
+            ?>
                 <div class="container">
                     <div class="row">
                         <div class="col-md-5 col-md-offset-3">
-                            <form action="#" method="post">
+                            <form action="#" method="POST">
                                 <div class="form-group">
                                     <label for="name">Your name</label>
-                                    <input class="form-control" type="text" id="name" name="name" required>
+                                    <input class="form-control" type="text" id="name" name="name">
                                 </div>
                                 <div class="form-group">
                                     <label for="username">Username</label>
-                                    <input class="form-control" type="text" id="username" name="username" required>
+                                    <input class="form-control" type="text" id="username" name="username" >
                                 </div>
                                 <div class="form-group">
                                     <label for="email">Email Address</label>
-                                    <input class="form-control" type="text" id="email" name="email" required>
+                                    <input class="form-control" type="text" id="email" name="email" >
                                 </div>
                                 <div class="form-group">
                                     <label for="password">Password</label>
-                                    <input class="form-control" type="text" id="password" name="password" required>
+                                    <input class="form-control" type="text" id="password" name="password" >
                                 </div>
-                                <button class="btn btn-danger" type="submit" name="register">SUbmit</button>
+                                <button class="btn btn-danger" type="submit" name="register">Submit</button>
                             </form>
                         </div>
                     </div>
