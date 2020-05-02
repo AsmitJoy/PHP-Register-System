2f1b3336e2db2ff2f1f538a7677feb9da8c95b56
Asmit Das Joy <asmitjoy17@gmail.com>
2020-05-02 14:37:23 +0600

    User Login Mechanism

--- Session.php
+++ Session.php
@@ -1,10 +1,26 @@
 <?php
     class Session{
-        public function __construct() {
-            echo"Hello";
+        public static function init(){
+            if(version_compare(phpversion(),'5.4.0','<')){
+                if(session_id()==''){
+                    session_start();
+                }
+            }else{
+                if(session_status()== PHP_SESSION_NONE){
+                    session_start();
+                }
+            }
         }
-    
-
+        public static function set($key, $val){
+            $_SESSION[$key] = $val;
+        }
+        public static function get($key){
+            if (isset($_SESSION[$key])) {
+                return $_SESSION[$key];
+            }else{
+                return false;
+            }
+        }
     }
     
     
